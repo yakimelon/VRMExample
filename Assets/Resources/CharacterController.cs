31fc0f1da980b87a4ecd4d55a15f5c9037fe668b
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-09-17 19:20:57 +0900

    first commit

---
+++ Assets/Resources/CharacterController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+	public float Speed = 2f;
+	public float Thrust = 100;
+
+	private Rigidbody _rigidBody;
+	private Animator _animator;
+
+	private bool _onGround;
+
+	void Start() {
+		_rigidBody = GetComponent<Rigidbody>();
+		_animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+		if (!_onGround) return;
+		CalcMove();
+		CalcJump();
+	}
+    
+	void OnCollisionStay(Collision other) {
+		_onGround = true;
+	}
+
+	private void CalcMove() {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			_rigidBody.velocity = new Vector3(Speed, 0, 0);
+			transform.rotation = Quaternion.Euler(0, 90, 0);
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			_rigidBody.velocity = new Vector3(-Speed, 0, 0);
+			transform.rotation = Quaternion.Euler(0, 270, 0);
+		} else if (Input.GetKey(KeyCode.UpArrow)) {
+			_rigidBody.velocity = new Vector3(0, 0, Speed);
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			_rigidBody.velocity = new Vector3(0, 0, -Speed);
+			transform.rotation = Quaternion.Euler(0, 180, 0);
+		} else {
+			_animator.SetBool("Running", false);
+			return;
+		}
+        
+		_animator.SetBool("Running", true);
+	}
+
+	private void CalcJump() {
+		if (Input.GetKey(KeyCode.Space)) {
+			_rigidBody.AddForce(new Vector3(0, Thrust, 0));
+			_animator.SetBool("Jumping", true);
+			_onGround = false;
+		} else {
+			_animator.SetBool("Jumping", false);
+		}
+	}
+}
