00dbaea0185d1686c48c9a3fd26736351e64e2a2
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-09-17 22:09:38 +0900

    VRMロード処理の実装と、ディレクトリ整理

---
+++ Assets/Resources/VRMLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMLoader : MonoBehaviour {
+	void Start () {
+		var path="Assets/Resources/Alicia/AliciaSolid.vrm";
+		var go=VRM.VRMImporter.LoadFromPath(path);
+		go.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+		go.transform.rotation = Quaternion.Euler(0, 180, 0);
+	}
+}
