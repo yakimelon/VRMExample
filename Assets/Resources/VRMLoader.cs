1f5bb246f1fef4a51ab644d3485a7a37af8023c1
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-09-18 09:53:06 +0900

    webからVRMを読み込む処理を実装した

--- Assets/Resources/VRMLoader.cs
+++ Assets/Resources/VRMLoader.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VrmLoader : MonoBehaviour {
-	void Start () {
-		var go = LoadVrmForLocal();
-		SetProperties(go);
+	void Start() {
+		LoadVrmForWeb();
 	}
 
+	// ローカルからVRMを読み込む
 	private GameObject LoadVrmForLocal() {
 		var path="Assets/Resources/Alicia/AliciaSolid.vrm";
 		return VRM.VRMImporter.LoadFromPath(path);
 	}
 
-	private GameObject LoadVrmForWeb() {
-		var www = new WWW("https://s3-ap-northeast-1.amazonaws.com/vrm-list/okijo.vrm");
-		return VRM.VRMImporter.LoadFromBytes(www.bytes);
+	// WebからVRMを読み込む
+	private void LoadVrmForWeb() {
+		var path = "https://s3-ap-northeast-1.amazonaws.com/vrm-list/okijo.vrm";
+
+		StartCoroutine(LoadVrmCoroutine(path, go =>
+		{
+			SetProperties(go);
+		}));
 	}
 
+	// 読み込んだVRMのプロパティ設定を行う
 	private void SetProperties(GameObject go) {
 		// 角度を設定して、Playerオブジェクトの子にセット
 		var player = GameObject.FindGameObjectWithTag("Player");
+		player.GetComponent<CharacterController>().enabled = true;
 		go.transform.parent = player.transform;
 		go.transform.rotation = Quaternion.Euler(0, 180, 0);
 		
 		// アニメーター動的セット
 		go.GetComponent<Animator>().runtimeAnimatorController = (RuntimeAnimatorController)Instantiate(Resources.Load ("Character"));
+	}
+	
+	// コルーチンでVRMを読み込む
+	IEnumerator LoadVrmCoroutine(string path, System.Action<GameObject> onLoaded) {
+		var www = new WWW(path);
+		yield return www;
+		VRM.VRMImporter.LoadVrmAsync(www.bytes, onLoaded);
 	}
 }
